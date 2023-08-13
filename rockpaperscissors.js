b4586ad4cad4d1e044680c7265e33b889304f77e
garamgim <garamgim@gmail.com>
2023-08-13 13:31:22 +0900

    Add UI design
    - Rock, paper, scissors emoji according to user & computer selection
    - Profile images changing according to winning status
    - Media queries (2 sizes)
    - Background images and font colors, etc.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -8,7 +8,9 @@ function clicked(e) {
 
 buttons.forEach(button => button.addEventListener('click', clicked));
 
-// Game Logic
+
+
+// Game 
 
 function playRound(player, computer) {
     player = player.toLowerCase();
@@ -32,9 +34,33 @@ function playRound(player, computer) {
     }
 }
 
+
+
+function changeEmoji(str, id) {
+
+    let emoji = document.getElementById(id);
+
+    switch (str.toLowerCase()) {
+
+        case ("rock"):
+            emoji.innerHTML = "&#x270A";
+            break;
+        case ("paper"):
+            emoji.innerHTML = "&#x270B";
+            break;
+        default:
+            emoji.innerHTML = "&#x270C";
+            break;
+
+    }
+
+}
+
+
 function getComputerChoice(arr) {
     return arr[Math.floor(Math.random() * 3)];
 }
+
 
 let playerScoreInt = 0;
 let computerScoreInt = 0;
@@ -43,12 +69,18 @@ let playerScorePrint = document.getElementById('playerscore');
 let computerScorePrint = document.getElementById('computerscore');
 let centerScoreBoard = document.getElementById('winner');
 
+let playerImg = document.getElementById('playerprofile');
+let computerImg = document.getElementById('computerprofile');
+
 
 function game(playerSelection) {
 
     const rps = ["rock", "paper", "scissors"];
-
     let computerSelection = getComputerChoice(rps);
+
+    changeEmoji(playerSelection, "playerhand");
+    changeEmoji(computerSelection, "computerhand");
+
     let result = playRound(playerSelection, computerSelection);
 
     switch (true) {
@@ -56,11 +88,24 @@ function game(playerSelection) {
         case (playerScoreInt < 4 && computerScoreInt < 4):
 
             if (result == "You Win!") {
+
                 playerScoreInt++;
                 playerScorePrint.innerHTML = playerScoreInt;
+                playerImg.src = "image/playerwin.png";
+                computerImg.src = "image/computerlose.png";
+
             } else if (result == "You Lose!") {
+
                 computerScoreInt++;
                 computerScorePrint.innerHTML = computerScoreInt;
+                playerImg.src = "image/playerlose.png";
+                computerImg.src = "image/computerwin.png";
+
+            } else {
+
+                playerImg.src = "image/playerlose.png";
+                computerImg.src = "image/computerlose.png";
+
             }
             centerScoreBoard.innerHTML = result;
             break;
@@ -71,6 +116,8 @@ function game(playerSelection) {
             playerScoreInt++;
             playerScorePrint.innerHTML = playerScoreInt;
             centerScoreBoard.innerHTML = "WINNER: YOU!";
+            playerImg.src = "image/playerwin.png";
+            computerImg.src = "image/computerlose.png";
             break;
 
 
@@ -78,7 +125,9 @@ function game(playerSelection) {
 
             computerScoreInt++;
             computerScorePrint.innerHTML = computerScoreInt;
-            centerScoreBoard.innerHTML = "WINNER: VILLAIN";
+            centerScoreBoard.innerHTML = "WINNER: VILLAIN...";
+            playerImg.src = "image/playerlose.png";
+            computerImg.src = "image/computerwin.png";
             break;
 
 
